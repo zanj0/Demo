6a14c59f76bcb45e953680635bd00423171a63ba
zanj0 <agnibhachakraborty@gmail.com>
2022-06-30 01:27:46 +0530

    Add Student Model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +19,12 @@ public class DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return com.sun.tools.javac.util.List.of("Hello", "World");
+	public List<Student> hello(){
+		return com.sun.tools.javac.util.List.of(
+			new Student(1L,
+						"Agnibha",
+						LocalDate.of(1998,2,17),
+						"agnibha@gmail.com",21 )
+		);
 	}
 }
